0070abba47e3f9e5d7e771c2cfb92368265d0228
davewalk <daviddwalk@gmail.com>
2013-09-22 11:56:16 -0400

    trying again

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var Sensors = Sensors || {};
 
   var dataLayer = new L.FeatureGroup();
 
-  $.getJSON('septa-climate-tracker/data/output.geojson', function (data) {
+  $.getJSON('data/output.geojson', function (data) {
 
     var humiditySum = _.reduce(data.features, function (memo, point) { return memo + point.properties.humid; }, 0);
     var humidityAvg = humiditySum / data.features.length;
