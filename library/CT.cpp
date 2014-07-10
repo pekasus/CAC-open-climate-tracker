46219eedcc2c4159f0ac075862e872b652a52dcd
Barbara <bdonnini9055@users.noreply.github.com>
2014-07-09 20:39:37 -0400

    Update CT.cpp

--- library/CT.cpp
+++ library/CT.cpp
@@ -1,1 +1,14 @@
 // ClimateTracker Library CPP file
+
+//Reading in libraries
+#include "EEPROM.h"
+#include "Arduino.h"
+
+void CTClass::incrEEPROM(int addr)
+{
+byte a = EEPROM.read(addr);
+a++;
+EEPROM.write(addr,a);
+}
+
+CTClass CT;
